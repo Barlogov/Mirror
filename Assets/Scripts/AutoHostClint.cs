ff85b28b7fd1e6110f06f45067da4dbdbd9ca8a7
Barlogov <danil2001sv@mail.ru>
2022-03-15 15:47:04 +0300

    First

---
+++ Assets/Scripts/AutoHostClint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AutoHostClint : MonoBehaviour
+{
+    [SerializeField] NetworkManager networkManager;
+
+    public void JoinLocalHost()
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StartClient();
+    }
+}
